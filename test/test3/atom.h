7f458cba62bfcc8633685c5d852dd56ddf1c2c6f
Zhihang Liang <command1748165360@126.com>
2022-08-06 12:10:44 +0800

    qylWheels: 增加了测试3

---
+++ test/test3/atom.h
@@ -0,0 +1,21 @@
+
+/**
+ * 文件名：atom.h
+ * 作者：qylWheels
+ * 日期：2022年7月29日
+ * 功能：提供接口
+ */
+
+#ifndef _ATOM_H_
+#define _ATOM_H_
+
+// 对外提供的接口
+extern       int   atom_length(const char *str);
+extern const char *atom_string(const char *str);
+extern const char *atom_int   (long n);
+
+/* XXX: 测试用的接口 */
+extern       void atom_get_distribution(int total_cnt);
+
+#endif
+
